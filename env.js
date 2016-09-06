ff220257a1a3098fde72bfb105c4ec12b0e059b3
Isac Lagerblad <isac.lagerblad@creuna.se>
2016-09-06 20:27:39 +0200

    created marvin

---
+++ env.js
@@ -0,0 +1,4 @@
+process.env['clientId'] = "12824820052.76295409846";
+process.env['clientSecret'] = "c1564f03a44c8dd3f325fe0e8a1d296f";
+process.env['port'] = 4114;
+process.env['REDIS_URL'] = "redis://h:pdrsjsma34unqmeqg09n6nkn0nk@ec2-54-163-236-98.compute-1.amazonaws.com:26679";
